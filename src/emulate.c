a3590dd953b08a075fadbb0a0f6583c01359b1bb
RyanOD <ryan7od@hotmail.com>
2024-05-28 16:52:05 +0100

    Data reading in started, args fixed, WIP (Infinite loop on reading)

--- src/emulate.c
+++ src/emulate.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define strlen(arr) = sizeOf(arr)/sizeOf(arr[0])
+#define strlen(arr) = sizeof(arr)/sizeof(arr[0])
 
 //ADTs
 typedef int64_t Register;
 
 struct PSTATE {
-  bool N = false;
-  bool Z = true;
-  bool C = false;
-  bool V = false;
+  bool N;
+  bool Z;
+  bool C;
+  bool V;
 };
 
 struct GeneralRegisters {
-  Register registers[32] = { 0 };
+  Register registers[32];
 };
 
 struct SpecialRegisters {
-  Register PC = 0;
-  Register Zero = 0;
+  Register PC;
+  Register Zero;
   struct PSTATE pstate;
 };
 
@@ -35,30 +35,43 @@ extern int64_t read64(Register* reg);
 //Global variables
 //2MB of memory
 unsigned char memory[2097152] = { 0 };
-struct GeneralRegisters gRegisters;
-struct SpecialRegisters sRegisters;
+struct GeneralRegisters gRegisters = { { 0 } };
+struct SpecialRegisters sRegisters= { 0, 0, { false, true, false, false } };
 
 int main(int argc, char **argv) {
   //Check 1 or 2 arguments
-  if (argc != 1 && argc != 2) {
+  if (argc != 3 && argc != 2) {
     printf("Wrong number of arguments");
     return -1;
   }
 
   //Set output method
   FILE *outPtr = stdout;
-  if (argc > 1) {
-    outPtr = fopen(argv[1], "w");
+  if (argc > 2) {
+    outPtr = fopen(argv[2], "w");
     if (outPtr == NULL) {
-      printf("Error opening %s", argv[1]);
+      printf("Error opening %s", argv[2]);
       return -1;
     }
   }
 
   //Read in file
-  FILE *inPtr = fopen(argv[0], "rb");
+  FILE *inPtr = fopen(argv[1], "rb");
+  unsigned char buffer[4] = { 0 };
+  unsigned char* memPtr = memory;
+  fread(buffer, sizeof(buffer), 1, inPtr);
+  while (buffer[0] != -1) {
+    *memPtr++ = buffer[0];
+    *memPtr++ = buffer[1];
+    *memPtr++ = buffer[2];
+    *memPtr++ = buffer[3];
+    fread(buffer, sizeof(buffer), 1, inPtr);
+  }
 
-  while ()
+  for(int i = 0; i < 8; i++) {
+    printf("%u ", memory[i]);
+    if ((i+1)%4 == 0) printf("\n");
+  }
 
 
   printEnd(outPtr);
