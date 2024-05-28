4dc86ad6176298122b90068fa8c3d9efe24da711
RyanOD <ryan7od@hotmail.com>
2024-05-28 14:14:22 +0100

    ADTs and global values for registers and memoery defined

--- src/emulate.c
+++ src/emulate.c
@@ -1,22 +1,50 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define strlen(arr) = sizeOf(arr)/sizeOf(arr[0])
+
+//ADTs
+typedef uint64_t Register;
+
+struct PSTATE {
+  bool N = false;
+  bool Z = true;
+  bool C = false;
+  bool V = false;
+};
+
+struct GeneralRegisters {
+  Register registers[32] = { 0 };
+};
+
+struct SpecialRegisters {
+  Register PC = 0;
+  Register Zero = 0;
+  struct PSTATE pstate;
+};
 
 //Prototype functions
 extern void printEnd(FILE *ptr);
 
-
+//Global variables
+//2MB of memory
+char* memory[2097152] = { 0 };
+struct GeneralRegisters gRegisters;
+struct SpecialRegisters sRegisters;
 
 int main(int argc, char **argv) {
   //Check 1 or 2 arguments
-  if (argc != 1 && argc != 2) return -1;
+  if (argc != 1 && argc != 2) {
+    printf("Wrong number of arguments");
+    return -1;
+  }
 
   //Set output method
   FILE *outPtr = stdout;
   if (argc > 1) {
-    outPtr = fopen(argv[1], 'w');
+    outPtr = fopen(argv[1], "w");
     if (outPtr == NULL) {
       printf("Error opening %s", argv[1]);
       return -1;
