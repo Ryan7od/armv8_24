b09a492a9139af22586c99c051f3675471274981
VeerV11 <veervishalvij@gmail.com>
2024-05-29 16:10:18 +0100

    initialised parser, and the internal representation. Fixed the symbol table and have implemented the add function for the symbol table - Veer

--- src/assemble.c
+++ src/assemble.c
@@ -1,10 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-struct list {
-    int *data;
-    int numItems;
-    int size;
-};
 
 struct SA_pair {
     char Symbol[100];
@@ -12,14 +7,25 @@ struct SA_pair {
 };
 
 
+struct list {
+    struct SA_pair *data;
+    int numItems;
+    int size;
+};
+
+typedef struct {} IR;
+
+
+
 void addToTable(struct list *mySymbolTable, struct SA_pair new_symbol);
+IR *parser(char *line, int *data);
 
 
 int main(int argc, char **argv) {
     struct list SymbolTable;
     SymbolTable.numItems = 0;
     SymbolTable.size = 20;
-    SymbolTable.data = calloc(SymbolTable.size, sizeof(struct SA_pair));
+    SymbolTable.data = malloc(SymbolTable.size * sizeof(struct SA_pair));
 
     if (SymbolTable.data == NULL) {
         printf("memory allocation failed");
@@ -27,6 +33,17 @@ int main(int argc, char **argv) {
     }
 
   return EXIT_SUCCESS;
-}
+};
 
+void addToTable(struct list *mySymbolTable, struct SA_pair new_symbol) {
+    if (mySymbolTable->numItems == mySymbolTable->size) {
+        mySymbolTable->size *= 2;
+        mySymbolTable->data = realloc(mySymbolTable->data, mySymbolTable->size * sizeof(struct SA_pair));
+        if (mySymbolTable->data == NULL) {
+            printf("Symbol table failed to be resized");
+        }
+    }
+    mySymbolTable->data[mySymbolTable->numItems] = new_symbol;
+    mySymbolTable->numItems++;
+}
 
