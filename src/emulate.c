d800d602120b83a103cc6b8b2f534d0a8cf0ac51
Max Intellij Laptop <ms4623@ic.ac.uk>
2024-05-30 16:24:08 +0100

    Added readability to op0 selection

--- src/emulate.c
+++ src/emulate.c
@@ -10,18 +10,18 @@
 //ADTs
 typedef uint64_t Register;
 
-struct PSTATE {
+typedef struct {
   bool N;
   bool Z;
   bool C;
   bool V;
-};
+} PSTATE;
 
-struct SpecialRegisters {
+typedef struct  {
   Register PC;
   Register Zero;
-  struct PSTATE pstate;
-};
+  PSTATE pstate;
+} SpecialRegisters;
 
 //Prototype functions
 extern void printEnd(FILE *ptr);
@@ -39,13 +39,13 @@ extern uint32_t twos(uint32_t num);
 //Global variables
 //2MB of memory
 unsigned char memory[MB2] = { 0 };
-Register gRegisters[31] = { 0 }; // Note Max changed from 32 to 31
-struct SpecialRegisters sRegisters = { 0, 0, { false, true, false, false } };
+Register gRegisters[31] = { 0 };
+SpecialRegisters sRegisters = { 0, 0, { false, true, false, false } };
 
 int main(int argc, char **argv) {
   //Ensure 1 or 2 arguments
   if (argc != 3 && argc != 2) {
-    printf("Wrong number of arguments");
+    printf("Error: wrong number of arguments");
     return EXIT_FAILURE;
   }
 
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
   if (argc > 2) {
     outPtr = fopen(argv[2], "w");
     if (outPtr == NULL) {
-      printf("Error opening %s", argv[2]);
+      printf("Error: opening %s", argv[2]);
       return EXIT_FAILURE;
     }
   }
@@ -78,31 +78,35 @@ int main(int argc, char **argv) {
 
   //Terminate at halt (8a000000)
   while(instruction != 0x8a000000) {
+    //Defined mask & isolate bits 28-5 to get op0
+    uint32_t mask = 0xF << 25;
+    uint32_t op0 = (instruction & mask) >> 25;
     //Switch case to send to different operation handlers
-    switch (instruction & 0b00011110000000000000000000000000) {
+    switch (op0) {
       //100x Data Processing (Immediate)
-      case (0b00010000000000000000000000000000):
-      case (0b00010010000000000000000000000000):
+      case (0b1000):
+      case (0b1001):
         dataProcessingImmHandler(instruction);
         break;
       //x101 Data Processing (Register)
-      case (0b00001010000000000000000000000000):
-      case (0b00011010000000000000000000000000):
+      case (0b0101):
+      case (0b1101):
         dataProcessingRegHandler(instruction);
         break;
       //x1x0 Loads and Stores
-      case (0b00011000000000000000000000000000):
-      case (0b00001100000000000000000000000000):
-      case (0b00001000000000000000000000000000):
-      case (0b00011100000000000000000000000000):
+      case (0b0100):
+      case (0b0110):
+      case (0b1100):
+      case (0b1110):
         loadStoreHandler(instruction);
         break;
       //101x Branches
-      case (0b00010100000000000000000000000000):
-      case (0b00010110000000000000000000000000):
+      case (0b1010):
+      case (0b1011):
         branchHandler(instruction);
         break;
       default:
+        printf("Error: Non implemented instruction");
         break;
     }
 
