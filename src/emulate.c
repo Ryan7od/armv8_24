2edeb0bba25b029e1966f4402bc1fcaf05876bdb
Max Stoddard <max.stoddard23@imperial.ac.uk>
2024-06-06 18:17:25 +0100

    Testing 447/591 complete: Fixed improper PC changing in branch conditional

--- src/emulate.c
+++ src/emulate.c
@@ -123,7 +123,7 @@ int main(int argc, char **argv) {
         skip = true;
         break;
       default:
-        fprintf(stderr, "Error: Non implemented instruction %x\n", op0);
+        fprintf(stderr, "Error: Non implemented instruction: %x (main)\n", op0);
         return -4;
     }
 
@@ -677,6 +677,8 @@ void branchHandler(uint32_t instruction) {
     uint32_t location = mask32_AtoB_shifted(instruction, 9, 5);
     if (location != 0b11111) {
       write64(&sRegisters.PC, read64(&gRegisters[location]));
+    } else {
+      fprintf(stderr, "Error: Register jump to ZR; not implemented");
     }
     return;
   }
@@ -695,42 +697,46 @@ void branchHandler(uint32_t instruction) {
   if (diff == 0b010) {
     //Tackle condition
     bool condition = false;
-    switch (mask32_AtoB_shifted(instruction, 3, 0)) {
-      case (0b0000):
-        condition = sRegisters.pstate.Z == 1;
+    uint16_t encode = mask32_AtoB_shifted(instruction, 3, 0);
+    switch (encode) {
+      case (0b0000): // EQ
+        condition = (sRegisters.pstate.Z == 1);
         break;
-      case (0b0001):
+      case (0b0001): // NE
         condition = sRegisters.pstate.Z == 0;
         break;
-      case (0b1010):
+      case (0b1010): // GE
         condition = sRegisters.pstate.N == sRegisters.pstate.V;
         break;
-      case (0b1011):
+      case (0b1011): // LT
         condition = sRegisters.pstate.N != sRegisters.pstate.V;
         break;
-      case (0b1100):
+      case (0b1100): // GT
         condition = sRegisters.pstate.Z == 0 && sRegisters.pstate.N == sRegisters.pstate.V;
         break;
       case (0b1101):
         condition = !(sRegisters.pstate.Z == 0 && sRegisters.pstate.N == sRegisters.pstate.V);
         break;
-      case (0b01110):
+      case (0b01110): //
         condition = true;
         break;
       default:
+        fprintf(stderr, "Error: Not implemented branch conditional hex code: %x", encode);
         break;
     }
 
-    if (!condition) {
+    if (!condition) { // Move onto next PC address if condition not met
       write64(&sRegisters.PC, read64(&sRegisters.PC) + 4);
       return;
     }
 
+    uint32_t simm19 = mask32_AtoB_shifted(instruction, 23 ,5);
     //Find base shift
-    uint32_t shift = mask32_AtoB_shifted(instruction, 23 ,5) << 2;
+    uint64_t shift = simm19 << 2;
     //Extend sign if MSB is 1
-    shift = sExtend32(shift, 27);
-    write64(&sRegisters.PC, read64(&sRegisters.PC) + shift);
+    shift = sExtend64(shift, 20);
+    uint64_t newPC = read64(&sRegisters.PC) + shift;
+    write64(&sRegisters.PC, newPC);
     return;
   }
 }
