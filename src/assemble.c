8d48c926e632a7e0086bd0e00daf946bed63184c
VeerV11 <veervishalvij@gmail.com>
2024-06-07 01:27:04 +0100

    Arithmetic instruction parsing complete (tested on a few instructions from testing suite)

--- src/assemble.c
+++ src/assemble.c
@@ -30,23 +30,25 @@ typedef struct {
     char *operand[MAX_OPERANDS];
 } InstructionIR;
 
+typedef struct {
+    const char* mnemonic;
+    uint32_t opcode_bin;
+} OpcodeMapping;
+
 typedef void (*InstructionParser)(InstructionIR, FILE *);
 
 uint32_t data_processing_immediate_code = 1 << 28;
-uint32_t data_processing_register_code = 5 << 27;
+uint32_t data_processing_register_code = 5 << 25;
 
 typedef struct {
     const char* mnemonic;
     InstructionParser parser;
 } InstructionMapping;
 
-typedef struct {
-    const char* mnemonic;
-    uint32_t opcode_bin;
-} OpcodeMapping;
 
-OpcodeMapping *opcodeMapping;
-size_t opcode_map_size;
+
+
+
 
 
 void addToTable(struct list *mySymbolTable, struct SA_pair new_symbol);
@@ -67,15 +69,15 @@ void parseDataProcessing(InstructionIR instruction, FILE *file);
 
 // Parsing functions
 void parseBranch(InstructionIR instruction, FILE *file) {
-    printf("Parsing Branch instruction: %s\n", instruction);
+    printf("x");
 }
 
 void parseLoadStore(InstructionIR instruction, FILE *file) {
-    printf("Parsing Load/Store instruction: %s\n", instruction);
+    printf("x");
 }
 
 void parseDataProcessing(InstructionIR instruction, FILE *file) {
-
+    printf("x");
 }
 
 static uint32_t getOpcode(InstructionIR instructionIr, OpcodeMapping mapping[], size_t size);
@@ -84,6 +86,8 @@ static void parseTwoOperand(InstructionIR instruction, FILE *file);
 
 static void parser(char *line);
 static InstructionParser functionClassifier(InstructionIR instruction,  InstructionMapping* mappings, size_t mapSize);
+static uint32_t getReg(char *reg);
+static void parseArtihmetic(InstructionIR instruction, FILE *file, OpcodeMapping opcodeMapping[], size_t opcode_map_size);
 
 
 int main(int argc, char **argv) {
@@ -97,6 +101,15 @@ int main(int argc, char **argv) {
         printf("memory allocation failed");
         return 1;
     }
+
+    OpcodeMapping opcodeMapping[] = {
+            {"add", 0},
+            {"adds", 1 << 29},
+            {"sub", 1 << 30},
+            {"subs", 3 << 29}
+    };
+
+    size_t opcode_msize = sizeof(opcodeMapping) / sizeof(opcodeMapping[0]);
 
     // Create an array of instruction mappings
     InstructionMapping mappings[] = {
@@ -142,24 +155,20 @@ int main(int argc, char **argv) {
     instruction1.operand[0] = "x5";
     instruction1.operand[1] = "x6";
 
-    InstructionParser parser1 = functionClassifier(instruction1, mappings, mappingCount);
-
 
     FILE *binaryCode = fopen("code.bin", "wb");
     if (binaryCode == NULL) {
         fprintf(binaryCode, "file unable to open");
         return EXIT_FAILURE;
     }
-    parser1(instruction1, binaryCode);
-
-    *opcodeMapping = {
-            {"add", 0},
-            {"adds", 1 << 29},
-            {"sub", 1 << 30},
-            {"subs", 3 << 30}
-    };
-
-
+    InstructionIR instruction2;
+    instruction2.opcode = "sub";
+    instruction2.operand[0] = "w15";
+    instruction2.operand[1] = "w0";
+    instruction2.operand[2] = "#0x5a0";
+    instruction2.operand[3] = "lsl #12";
+    parseArtihmetic(instruction2, binaryCode, opcodeMapping, opcode_msize);
+    fclose(binaryCode);
    return EXIT_SUCCESS;
 };
 
@@ -256,33 +265,65 @@ static uint32_t getOpcode(InstructionIR instructionIr, OpcodeMapping mapping[], 
     exit(1);
 }
 
-static void parseArtihmetic(InstructionIR instruction, FILE *file) {
+static void parseArtihmetic(InstructionIR instruction, FILE *file, OpcodeMapping opcodeMapping[], size_t opcode_map_size) {
     uint32_t opcode_bin = getOpcode(instruction, opcodeMapping, opcode_map_size);
     uint32_t opi = 1 << 24;
     uint32_t sf = 1 << 31;
-    if (*instruction.operand[0] == 'W') {
+    if (*instruction.operand[0] == 'w' || *instruction.operand[0] == 'W') {
         sf = 0;
     }
     if (*instruction.operand[2] == '#') {
         char *startptr = instruction.operand[2] + 1;
-        char *endptr;
-        uint32_t imm12 = strtoul(startptr, &endptr, 10) << 21;
+        printf("%s\n", startptr);
+        uint32_t imm12 = (strtoul(startptr, NULL, 16)) << 10;
+        printf("%u\n", imm12);
         uint32_t sl = 0;
         if (strcmp(instruction.operand[3], "lsl #12") == 0) {
             sl = 1 << 22;
         }
-        uint32_t rn;
-        uint32_t rd;
-        uint32_t write_val = sf || opcode_bin || data_processing_immediate_code || opi || sl || imm12 || rn || rd;
+        uint32_t rn = getReg(instruction.operand[1]) << 5;
+        uint32_t rd = getReg(instruction.operand[0]);
+        uint32_t write_val = sf | opcode_bin | data_processing_immediate_code | opi | sl | imm12 | rn | rd;
         size_t written = fwrite(&write_val, sizeof (uint32_t), 1, file);
         if (written != 1) {
             printf("error writing to file");
             exit(1);
         }
+        printf("%u", write_val);
     } else {
-        //TODO
+        uint32_t M = 0;
+        uint32_t opr = 1 << 24;
+        uint32_t rm = getReg(instruction.operand[2]) << 16;
+        uint32_t rn = getReg(instruction.operand[1]) << 5;
+        uint32_t rd = getReg(instruction.operand[0]);
+        uint32_t operand = 0;
+        if (instruction.operand[3] != NULL) {
+            char *shift = malloc(3 * sizeof(char));
+            strncpy(shift, instruction.operand[3], 3);
+            if (strcmp(shift, "lsr") == 0) {
+                opr = 10 << 21;
+            } else if (strcmp(shift, "asr") == 0) {
+                opr = 12 << 23;
+            }
+            free(shift);
+            char *number = instruction.operand[3] + 5;
+            operand = (strtoul(number, NULL, 10)) << 10;
+            free(number);
+        }
+        uint32_t write_val = sf | opcode_bin | M | data_processing_register_code | opr | rm | operand | rn | rd;
+        size_t written = fwrite(&write_val, sizeof (uint32_t), 1, file);
+        if (written != 1) {
+            printf("error writing to file");
+            exit(1);
+        }
+        printf("%u", write_val);
     }
 
+}
+
+static uint32_t getReg(char *reg) {
+    uint32_t rv = strtoul((reg + (1*sizeof (char))), NULL, 10);
+    return rv;
 }
 
 
